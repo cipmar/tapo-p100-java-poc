620c0c531bc0c774e6cf5f75d49ec4001af757fc
Marius Ropotica <marius.ropotica@mambu.com>
2022-01-06 22:46:32 +0200

    Small improvements

--- src/main/java/com/github/k4czp3r/tapop100/helpers/KspDebug.java
+++ src/main/java/com/github/k4czp3r/tapop100/helpers/KspDebug.java
@@ -1,9 +1,10 @@
 package com.github.k4czp3r.tapop100.helpers;
 
+import java.util.Date;
+
 public class KspDebug {
 
-    public static void out(String content)
-    {
-        System.out.println(String.format("[TAPO-PoC] %s", content));
-    }
+	public static void out(String content) {
+		System.out.printf("[TAPO-PoC] %1$tF %1tT %s%n", new Date(), content);
+	}
 }
